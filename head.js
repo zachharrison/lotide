a33ee4903d9a6f17715f689d967ec4ce8224fb7e
Zach Harrison <zharrison100@gmail.com>
2020-11-23 23:23:12 +0000

    Head function added

---
+++ head.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = arr => arr[0];
+
+assertEqual(head([5,6,7]), 5);
