68954a2de4ec4771c25624630009da24ac0e2e22
Zach Harrison <zharrison100@gmail.com>
2020-11-25 23:40:30 +0000

    added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (obj, val) => {
+  const keys = Object.keys(obj);
+  const values = Object.values(obj);
+  let result = values
+  .map((x, i, a) => {
+    if(x === val){
+      return keys[i]
+    } else {
+      return;
+    }
+  })
+  .filter(x => x);
+  return result.length < 1 ? undefined : result.join('');
+  // .join('');
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
