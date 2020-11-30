af2a651a9c35b262b65f3b5f2a05267e8bfff734
Zach Harrison <zharrison100@gmail.com>
2020-11-30 22:05:32 +0000

    modularized code base

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,10 @@
-const eqArrays = function(arr1, arr2) {
-  return arr1.every((x, i, a) => x === arr2[i]);
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   eqArrays(arr1, arr2) ? 
   console.log('✅ The given arrays are identical') : 
   console.log('❌ The given arrays are not identical');
-}
+};
+
+module.exports = assertArraysEqual;
 
--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   return arr1.every((x, i, a) => x === arr2[i]);
 };
 
-console.log(eqArrays([1, 3, 5], [1, 3, 4]));
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+module.exports = eqArrays;
