9199d997356ae479071d18cd089de36b3fba6abb
Zach Harrison <zharrison100@gmail.com>
2020-11-30 22:45:19 +0000

    modularized code base

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,12 @@
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 const head = require('../head')
 
-assertEqual(head([5, 8, 9]), 5);
-assertEqual(head(['Z', 'a', 'c', 'h']), 'Z');
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,12 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const arr = ['Zach', 'Harrison'];
-assertEqual(tail(arr).join(''), 'Harrison');
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(tail(['5']), ['5']); 
+  });
+});
