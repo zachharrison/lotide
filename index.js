435acc74b0ba94bd0cbd8ef9bf0004bd3ae7aa95
Zach Harrison <zharrison100@gmail.com>
2020-11-30 23:37:52 +0000

    added index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const head = require('./head');
+const tail = require('./tail');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const middle = require('./middle');
+const takeUntil = require('./takeUntil')
+const without = require('./without')
+
+module.exports = {
+  head,
+  tail,
+  eqArrays,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  middle,
+  takeUntil, 
+  without
+};
